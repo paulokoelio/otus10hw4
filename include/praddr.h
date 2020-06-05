ce100e4ca8ca196541e322bb180822a462778f5a
Solverdce <solverdce@gmail.com>
2020-06-05 12:59:59 +0300

    Revert "remove all for review"
    This reverts commit 0edd78e33baff926cf026f4950a43f18b8201da1.

---
+++ include/praddr.h
@@ -0,0 +1,18 @@
+/**
+\file
+\brief Заголовочный файл общий для всей функции print_ip()
+
+Заголовочный файл с подключением всех остальных заголовочных файлов, 
+необходимых для включения функции print_ip() в код.
+*/
+#ifndef PRADDR_H_
+#define PRADDR_H_
+
+#include "version.h"
+#include "praddr_basefncl.h"
+#include "mylib_print_tuple.h"
+
+using namespace praddr;
+using namespace mylib;
+
+#endif /* PRADDR_BASEFNCL_H_ */
---
+++ src/praddr_basefncl.cpp
@@ -0,0 +1,16 @@
+/**
+\file
+\brief Реализация нешаблонных функций
+
+*/
+#include "praddr_basefncl.h"
+
+namespace praddr
+{
+
+    void prns()
+    {
+        std::cout << "type is not supported" << '\n';
+    }
+
+} // namespace praddr
